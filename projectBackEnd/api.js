38b4f089dd28123e028e5faaad3e645a4763c124
calvin smith <calvinl345@gmail.com>
2021-12-08 15:58:25 -0700

    First commit

---
+++ projectBackEnd/api.js
@@ -0,0 +1,66 @@
+////////// Imports //////////
+
+////////////////////////////////
+
+require("dotenv").config();
+const express = require("express");
+
+////////// Middleware //////////
+
+////////////////////////////////
+const app = express();
+app.use(express.json());
+
+////////// API Config //////////
+
+////////////////////////////////
+const port = process.env.PORT || 4002;
+app.listen(port, () => {
+    console.log(`server is live on port ${port}`);
+});
+
+////////// API Routes //////////
+
+////////////////////////////////
+
+//Get all restaurants
+app.get("/api/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "sucsess",
+        data: { restaurant: "toco bell" },
+    });
+});
+
+//Get one restaurant
+app.get("/api/restaurants/:id", (req, res) => {
+    res.status(200).json({
+        status: "sucsess",
+        data: { restaurant: "toco bell" },
+    });
+});
+
+//Create a restaurant
+app.post("/api/restaurants/create", (req, res) => {
+    console.log(req.body);
+    res.status(200).json({
+        status: "sucsess",
+        data: { restaurant: "toco bell" },
+    });
+});
+
+//Update a restaurant
+app.put("/api/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: "sucsess",
+        data: { restaurant: "toco bell" },
+    });
+});
+
+//Delete a restaurant
+app.delete("/api/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "sucsess",
+    });
+});
---
+++ projectBackEnd/db/index.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "dbuser",
+    host: "database.server.com",
+    database: "mydb",
+    password: "secretpassword",
+    port: 3211,
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
